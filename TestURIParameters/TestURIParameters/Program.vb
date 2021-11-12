a055f6c233583929dd98d118fe8d4c9f2d124f18
DeCaptain <kirissa2000@yahoo.com>
2021-11-12 15:29:27 -0800

    TestURIParameter
    tests protocol handler from web page

---
+++ TestURIParameters/TestURIParameters/Program.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Windows
+Imports System.Drawing
+
+Public Class Program
+
+    Public Shared Sub Main(ByVal args() As String)
+
+        If args.Length > 0 Then
+            For argsnum As Integer = 0 To UBound(args, 1)
+                Console.WriteLine(args(argsnum).ToString() & Environment.NewLine())
+
+            Next
+
+        Else
+            Console.WriteLine("argument is empty!")
+
+        End If
+
+        Console.Read()
+
+    End Sub
+
+
+End Class
